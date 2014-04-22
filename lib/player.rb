9db79c0864cb740f3d5cd207dff0dd1ae360f176
Oliver Delevingne <oliver@delevingne.co.uk>
2014-04-22 18:15:09 +0100

    floating ships test

--- lib/player.rb
+++ lib/player.rb
@@ -1,24 +1,28 @@
-require_relative './board.rb'
-
-board ||= Board.new
-@board = board
+# include 'board.rb'
 
 class Player
 
-def initialize(name)
-	@name ||= name
-end
+	include Board
 
-def board
-	@board
-end 
+	def initialize(name)
+		@name = name
+		@board = Board.new
+	end
 
-def name
-	@name
-end
+	def name
+		@name
+	end
+
+	def board
+		@board
+	end
 	
-def has_ships_still_floating?
-	board.include? ['s']
-end
+	def shoot(at_coordinates, opponent_board)
+		opponent_board.register_shots_at(at_coordinates)
+	end
+	
+	def has_ships_still_floating?(board)
+		board.hit_count < board.default_lives
+	end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,19 +3,17 @@ require 'player.rb'
 describe Player do
 
 	let(:player) {Player.new('Joey')}
-	let(:board) {double :array}
+	let(:player2) {Player.new('Colin')}
+	let(:board) {double :board}
 	
-it "has a name" do
-	expect(player.name).to eq'Joey'
-end
+	it "has a name" do
+		expect(player.name).to eq'Joey'
+	end
 
-it "should have a board" do
-	expect(player.board).not_to eq nil?
-end
+	it "knows if ships are still floating" do
+		expect(board).to receive(:hit_count)
+		player.has_ships_still_floating?
 
-it "should know if ships are on its board" do 
-	expect(player.has_ships_still_floating?).not_to eq true
-end
-
+	end
 end
 
