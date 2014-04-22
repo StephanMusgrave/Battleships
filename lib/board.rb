4e876ad5aac9ee822e803b90181144f2258d6342
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-04-22 13:17:19 +0100

    row and rows methods

---
+++ lib/board.rb
@@ -0,0 +1,12 @@
+class Board
+
+def row
+	@row = ["","","","","","","","","",""]
+end
+
+def rows
+	@rows = [@row,@row,@row,@row,@row,@row,@row,@row,@row,@row]
+
+end
+
+end
---
+++ spec/board_spec.rb
@@ -0,0 +1,15 @@
+require 'board'
+
+describe Board do
+
+	it 'a row should have 10 positions' do
+		board = Board.new
+		expect(board.row.count).to eq 10
+	end
+
+	it 'board should have 10 rows' do
+		board = Board.new
+		expect(board.rows.count).to eq 10
+	end
+
+end
