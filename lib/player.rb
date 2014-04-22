ed818f94dded5b08476c8273057e8cf66f1caaf8
Oliver Delevingne <oliver@delevingne.co.uk>
2014-04-22 14:55:53 +0100

    added has board method

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,24 @@
+require_relative './board.rb'
+
+board ||= Board.new
+@board = board
+
 class Player
 
 def initialize(name)
 	@name ||= name
 end
 
+def board
+	@board
+end 
+
 def name
 	@name
+end
+	
+def has_ships_still_floating?
+	board.include? ['s']
 end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,11 +3,19 @@ require 'player.rb'
 describe Player do
 
 	let(:player) {Player.new('Joey')}
-
+	let(:board) {double :array}
 	
 it "has a name" do
 	expect(player.name).to eq'Joey'
 end
 
+it "should have a board" do
+	expect(player.board).not_to eq nil?
+end
+
+it "should know if ships are on its board" do 
+	expect(player.has_ships_still_floating?).not_to eq true
+end
 
 end
+
